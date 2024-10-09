1edfbb2c50e731678ba4cb2ad9d4038e3db4248c
KhalisNugroho <khaliscahyonugroho171103@gmail.com>
2024-10-09 18:36:55 +0700

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,107 @@
+using Microsoft.AspNetCore.Mvc;
+using SecureWeb.Models;
+using SecureWeb.Data;
+using SecureWeb.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace SecureWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUser _userData;
+
+        public AccountController(IUser user)
+        {
+            _userData = user;
+        }
+
+        public IActionResult Index()
+        {  
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegistraionViewModel registrationViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var user = new User
+                    {
+                        Username = registrationViewModel.Username,
+                        Password = registrationViewModel.Password,
+                        RoleName = "contributor"
+                    };
+                    _userData.Registration(user);
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (System.Exception ex)
+                {
+                    ViewBag.Error = ex.Message;
+                }
+            }
+            return View(registrationViewModel);
+        }
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+
+            try
+            {
+                var user = new User
+                {
+                    Username = loginViewModel.Username,
+                    Password = loginViewModel.Password
+                };
+
+                var loginUser = _userData.Login(user);
+                if (loginUser == null)
+                {
+                    ViewBag.Error = "Invalid login attempt.";
+                    return View(loginViewModel);
+                }
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Username)
+                };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    principal,
+                    new AuthenticationProperties
+                    {
+                        IsPersistent = loginViewModel.RememberLogin
+                    });
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View(loginViewModel);
+        }
+    }
+}
---
+++ Data/IUser.cs
@@ -0,0 +1,10 @@
+using System;
+using SecureWeb.Models; 
+
+namespace SecureWeb.Data;
+
+public interface IUser
+{
+    User Registration(User user);
+    User Login(User user);
+}
